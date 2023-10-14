59f4b6f3855207bb86ce06eaf037134d8f8bbddd
Paul Martin <greatestloginnameever@gmail.com>
2023-10-14 17:51:00 +0100

    Fix Github Actions TextDecoder issue

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,8 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@testing-library/jest-dom';
-import { TextEncoder } from 'util';
+import { TextEncoder, TextDecoder } from 'util';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Request } from 'node-fetch';
 
+(global as any).TextDecoder = TextDecoder;
 global.TextEncoder = TextEncoder;
 global.Request = Request as any;
